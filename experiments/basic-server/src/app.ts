ea4ec6df9f7830edd9e4165708225f46a079c78f
pierrechc <pierre.chiche@protonmail.com>
2020-04-10 18:40:22 +0200

    Conf: Add and configure Jest dependency

--- experiments/basic-server/src/app.ts
+++ experiments/basic-server/src/app.ts
@@ -8,13 +8,22 @@ import conf from './conf'
 const app = express();
 app.use(bodyParser.json());
 
+//const router = express.Router();
+
 app.get('/', (req, res) => {
   res.send('hi');
 });
 
+// Sensor routes
 app.post('/sensor', (req,res) => {
-  res.send(`user id ${req.body['id']} sent ${req.body}`)
+  res.send(`user id ${req.params['id']} sent ${req.body}`)
 });
+
+// "Analytics" routes
+app.get('/user/:user_id', (req, res) => {
+  res.send(`user id ${req.params['user_id']} wants to know his/her score`)
+})
+
 
 app.listen(conf.PORT, () => {
   console.log('Server started!')
